666479d64886f766f3fda90c04081df04206abd5
ycopp9 <aciees95@gmail.com>
2023-03-20 23:29:21 +0900

    add SLinkedList, ArrayStack, LinkedStack

---
+++ src/main/java/dataStructure/LinkedStack.java
@@ -0,0 +1,38 @@
+package dataStructure;
+
+public class LinkedStack {
+    SLinkedList S;
+    int cnt;
+    LinkedStack() {
+        S = new SLinkedList();
+    }
+
+    void push(int e) {
+        S.addFirst(e);
+        cnt++;
+    }
+
+    int pop() {
+        if (cnt == 0) {
+            throw new RuntimeException("StackEmpty");
+        }
+        int value = S.removeFirst();
+        cnt--;
+        return value;
+    }
+
+    int top() {
+        if (cnt == 0) {
+            throw new RuntimeException("StackEmpty");
+        }
+        return S.getFirst();
+    }
+
+    int size() {
+        return cnt;
+    }
+
+    boolean empty() {
+        return cnt == 0;
+    }
+}
