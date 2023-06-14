2924880dd1400808cddc62a8f046ea7c713f1b3c
ycopp9 <aciees95@gmail.com>
2023-06-15 00:00:37 +0900

    LinkedQueue 구현

--- src/main/java/dataStructure/LinkedQueue.java
+++ src/main/java/dataStructure/LinkedQueue.java
@@ -1,5 +1,40 @@
 package dataStructure;
 
 public class LinkedQueue {
+    CircleList C;
+    int n;
+    
+    LinkedQueue() {
+    }
 
+    int front() {
+        if (empty()) {
+            throw new RuntimeException("QueueEmpty");
+        }
+        return C.front();
+    }
+
+    int dequeue() {
+        if (empty()) {
+            throw new RuntimeException("QueueEmpty");
+        }
+        int f = C.front();
+        C.remove();
+        n--;
+        return f;
+    }
+
+    void enqueue(int e) {
+        C.add(e);
+        C.advance();
+        n++;
+    }
+
+    int size() {
+        return n;
+    }
+
+    boolean empty() {
+        return n == 0;
+    }
 }
